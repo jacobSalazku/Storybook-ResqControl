136fc530577104e395a54a1996a1ec96cb26cba7
jacobSalazku <Cooljss10@gmail.com>
2023-11-27 14:20:13 +0100

    updating completion  button treatment card

--- src/Components/helpers.tsx
+++ src/Components/helpers.tsx
@@ -2,6 +2,8 @@ import lightBlueCheck from '../stories/helpers/icons/lightBlueCheck.png';
 import Icon from './Icon';
 import IconButton from './IconButton';
 import {
+    IconArrowBarRight,
+    IconCheck,
     IconChevronDown,
     IconChevronUp,
     IconGenderFemale,
@@ -19,42 +21,10 @@ export const getGender = (gender: string) => {
 // eslint-disable-next-line
 export const checkIfCompleted = (isCompleted: boolean) => {
     if (!isCompleted) {
-        return (
-            <IconButton
-                variant="rounded-full"
-                backgroundColor="bg-[#497DAE] border-[#497DAE]"
-                size="w-9 h-9"
-                icon={
-                    <Icon
-                        icon="icon-tabler-arrow-narrow-right"
-                        color="#FFFFFF"
-                        height="30"
-                        width="30"
-                        path={
-                            <>
-                                <path
-                                    stroke="none"
-                                    d="M0 0h24v24H0z"
-                                    fill="none"
-                                />
-                                <path d="M5 12l14 0" />
-                                <path d="M15 16l4 -4" />
-                                <path d="M15 8l4 4" />
-                            </>
-                        }
-                    />
-                }
-            />
-        );
+        return <IconArrowBarRight color="#497DAE" size={36} />;
     }
 
-    return (
-        <img
-            className="cursor-pointer"
-            src={lightBlueCheck}
-            alt="completed"
-        ></img>
-    );
+    return <IconCheck color="#497DAE" size={36} />;
 };
 
 export const pointArrow = (show: boolean) => {
